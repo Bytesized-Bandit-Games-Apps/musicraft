9fc0a02e4595245a6bcc2f34dbcf27b47ef1133f
Todd Mahoney <euphoricsoulrave@gmail.com>
2020-09-21 19:33:24 -0600

    starting point with rspec
    first 3 passing specs written

--- lib/musicraft.rb
+++ lib/musicraft.rb
@@ -1,6 +1,19 @@
 require "musicraft/version"
 
 module Musicraft
+  class Key
+    attr_accessor :note, :scale
+    NOTES = Array["C", "C#_Db", "D", "D#_Eb", "E", "F", "F#_Gb", "G", "G#_Ab", "A", "A#_Bb", "B"]
+
+    def initialize(note, scale)
+      @note = note
+      @scale = scale
+    end
+    def number_of_musical_notes
+      NOTES.size()
+    end
+
+  end
   class Error < StandardError; end
   # Your code goes here...
 end
